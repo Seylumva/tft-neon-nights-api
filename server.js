7cb5db3db538b85f1dbceb913ded26522bfb5e9e
SeylumVA <sc2nem@gmail.com>
2022-02-20 13:35:21 -0500

    Added index route

--- server.js
+++ server.js
@@ -13,6 +13,43 @@ app.listen(port, () => {
   console.log(`Now live at http://localhost:${port}/`);
 });
 
+app.get("/", (req, res) => {
+  res.json({
+    routes: [
+      {
+        url: "/api/champions",
+        queries: [
+          {
+            cost: {
+              min: 1,
+              max: 5,
+              required: false,
+            },
+            hasClass: {
+              required: false,
+            },
+            hasOrigin: {
+              required: false,
+            },
+          },
+        ],
+      },
+      {
+        url: "/api/classes",
+      },
+      {
+        url: "/api/origins",
+      },
+      {
+        url: "/api/items",
+      },
+      {
+        url: "/api/traits",
+      },
+    ],
+  });
+});
+
 app.use((err, req, res, next) => {
   const { message = "Something went wrong.", statusCode = 500 } = err;
   res.status(statusCode).json({
